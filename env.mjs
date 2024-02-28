233314bc3230fc140bfc0d6ef2a74306eb4b1d42
Michael <enjoyweaver@gmail.com>
2024-02-28 16:54:48 -0600

    again

--- env.mjs
+++ env.mjs
@@ -14,6 +14,7 @@ export const env = createEnv({
 
     STRIPE_API_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string().min(1),
+    STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
@@ -28,7 +29,7 @@ export const env = createEnv({
 
     STRIPE_API_KEY: process.env.STRIPE_API_KEY,
     STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-
+    STRIPE_PRO_MONTHLY_PLAN_ID: process.env.STRIPE_PRO_MONTHLY_PLAN_ID,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
