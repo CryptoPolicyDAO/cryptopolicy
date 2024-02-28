eb5c246df71049685641119ad94c92de0a04d7ea
Michael <enjoyweaver@gmail.com>
2024-02-28 16:58:22 -0600

    we'll see

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
+  name: "Crypto Policy DAO",
   description:
-    "An open source application built using the new router, server components and everything new in Next.js 13.",
+    "Where developers and legislators lobby for web3 friendly regulation.",
   url: "https://tx.shadcn.com",
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
