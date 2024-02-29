c0c75b9ee87d1ddafa7b249a0eee3ab88167021a
Michael <enjoyweaver@gmail.com>
2024-02-29 11:35:25 -0600

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,22 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ["@prisma/client"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next",
+          outputPath: "static/images/",
+          name: "[name].[hash].[ext]",
+          esModule: false,
+        },
+      },
+    })
+
+    return config
+  },
 }
 
 export default withContentlayer(nextConfig)
