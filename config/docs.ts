e56ff40680ca14740c9fa038bb059f1a1712c25f
Michael <enjoyweaver@gmail.com>
2024-03-02 19:49:55 -0600

    docs

--- config/docs.ts
+++ config/docs.ts
@@ -22,7 +22,121 @@ export const docsConfig: DocsConfig = {
       ],
     },
     {
-      title: "Regulation Matrix",
+      title: "Regulation Matrix - Decentralized",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs/documentation",
+        },
+        {
+          title: "Electricity Layer",
+          href: "/docs/documentation/electricity",
+          disabled: false,
+        },
+        {
+          title: "Settlement Layer",
+          href: "/docs/documentation/components",
+          disabled: false,
+        },
+        {
+          title: "Node / Validator",
+          href: "/docs/documentation/code-blocks",
+          disabled: false,
+        },
+        {
+          title: "Consensus Layer - Computation Intensive-based",
+          href: "/docs/documentation/style-guide",
+          disabled: false,
+        },
+        {
+          title: "Consensus Layer - Capabilities-based",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Consensus Layer - Voting-based",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Transaction Layer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "On-chain Data Storage Layer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Asset Layer - Stablecoins",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Asset Layer - Fungible Tokens",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Asset Layer - Non-Fungible Tokens",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Exchange Layer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Protocol Layer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Application Layer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Permanent Data Storage Layer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Internet Browser",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Developer",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Programming Language Auditor",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "DeFi Architecture Security",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "DeFi Architecture Standards / Auditor",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+        {
+          title: "Fiat Onboarding / Offboarding Access",
+          href: "/docs/in-progress",
+          disabled: false,
+        },
+      ],
+    },
+    {
+      title: "Regulation Matrix - Centralized",
       items: [
         {
           title: "Introduction",
