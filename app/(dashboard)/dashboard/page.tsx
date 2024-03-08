e339ea878a81f1e853aa99950411048aff814968
Michael <enjoyweaver@gmail.com>
2024-03-08 13:11:08 -0600

    dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import { redirect } from "next/navigation"
+import { createClient } from "@/utils/supabase/server"
 
 import { EmptyPlaceholder } from "@/components/empty-placeholder"
 import { DashboardHeader } from "@/components/header"
-import { PostCreateButton } from "@/components/post-create-button"
-import { PostItem } from "@/components/post-item"
 import { DashboardShell } from "@/components/shell"
 
 export const metadata = {
@@ -11,11 +10,30 @@ export const metadata = {
 }
 
 export default async function DashboardPage() {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect("/")
+  }
+
   return (
     <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <PostCreateButton />
-      </DashboardHeader>
+      <DashboardHeader
+        heading="Posts"
+        text="Create and manage posts."
+      ></DashboardHeader>
+      <div>
+        <div className="divide-y divide-border rounded-md border"></div>
+
+        <EmptyPlaceholder>
+          <EmptyPlaceholder.Icon name="post" />
+          <EmptyPlaceholder.Title>No posts created</EmptyPlaceholder.Title>
+          <EmptyPlaceholder.Description>
+            You don&apos;t have any posts yet. Start creating content.
+          </EmptyPlaceholder.Description>
+        </EmptyPlaceholder>
+      </div>
     </DashboardShell>
   )
 }
