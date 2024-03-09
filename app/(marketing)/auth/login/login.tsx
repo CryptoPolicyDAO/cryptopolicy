75ba78abbcd094bdcd3c28201fdb30b1554dd696
Michael <enjoyweaver@gmail.com>
2024-03-09 11:50:59 -0600

    tailwind order

--- app/(marketing)/auth/login/login.tsx
+++ app/(marketing)/auth/login/login.tsx
@@ -35,16 +35,10 @@ export default function LoginPage() {
 
   return (
     <>
-      <p
-        className="quando-regular my-10 leading-normal text-muted-foreground text-2xl sm:text-2xl md:text-3xl lg:text-5xl "
-        style={{ textAlign: "center" }}
-      >
+      <p className="quando-regular my-10 leading-normal text-muted-foreground text-2xl sm:text-2xl md:text-3xl lg:text-5xl text-center">
         Thank you for contributing to web3-friendly regulation
       </p>
-      <p
-        className="quando-regular my-10 leading-normal text-muted-foreground text-2xl sm:text-2xl md:text-2xl lg:text-3xl"
-        style={{ textAlign: "center" }}
-      >
+      <p className="quando-regular my-10 leading-normal text-muted-foreground text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center">
         This is the Member NFT
       </p>
       <div style={{ width: "50%", margin: "0 auto" }}>
@@ -53,7 +47,7 @@ export default function LoginPage() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="mx-auto my-10 flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+      <div className="mx-auto my-10 flex flex-col items-center space-y-4 max-w-[58rem] text-center">
         <p className="quando-regular max-w-[85%] leading-normal text-muted-foreground text-2xl sm:text-2xl md:text-2xl lg:text-2xl">
           If you have not created an account, you can do so by entering your
           email and creating any password you want and hitting the sign up
@@ -61,6 +55,7 @@ export default function LoginPage() {
           log in with your email and password.
         </p>
       </div>
+
       <div
         style={{
           display: "flex",
