0d6a9c6a95ebd908ac04327e34084c40ea2a5188
Michael <enjoyweaver@gmail.com>
2024-03-09 16:47:31 -0600

    blog content

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,6 +3,10 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
+      title: "About",
+      href: "/#about",
+    },
+    {
       title: "Blog",
       href: "/blog",
     },
