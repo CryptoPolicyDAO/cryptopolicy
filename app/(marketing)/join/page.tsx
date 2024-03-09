ac7d450841f06e9abb8b8f0f05f306feb3824c31
Michael <enjoyweaver@gmail.com>
2024-03-09 14:59:58 -0600

    mobile styling

--- app/(marketing)/join/page.tsx
+++ app/(marketing)/join/page.tsx
@@ -5,64 +5,66 @@ export const metadata = {
 export default function Join() {
   return (
     <div className="quando-regular ">
-      <h1
-        className="mb-5 mt-10 text-center text-3xl leading-[1.1] sm:text-3xl md:text-6xl"
-        style={{ color: "var(--header-color)" }}
-      >
-        Contribute and Join
-      </h1>
-      <p
-        className="my-10 text-center text-2xl font-bold sm:text-2xl"
-        style={{ color: "var(--description-color)" }}
-      >
-        Join Crypto Policy DAO to enhance regulatory clarity and promote
-        blockchain innovation
-      </p>
-      <div>
-        <p
-          className="mx-auto mb-10 text-center text-xl"
-          style={{
-            color: "var(--content-color)",
-            maxWidth: "900px",
-          }}
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+        <h1
+          className="mb-5 mt-10 text-center text-3xl leading-[1.1] sm:text-3xl md:text-6xl"
+          style={{ color: "var(--header-color)" }}
         >
-          There are two ways to join Crypto Policy DAO; you can either
-          contribute financially or contribute to our open-source projects. You
-          can find more information about financially supporting the DAO under
-          the Support tab, though more information about contributing to our
-          open-source projects is provided below.
-        </p>
+          Contribute and Join
+        </h1>
         <p
-          className="mx-auto mb-10 text-center text-xl"
-          style={{
-            color: "var(--content-color)",
-            maxWidth: "900px",
-          }}
+          className="my-10 text-center text-2xl font-bold sm:text-2xl"
+          style={{ color: "var(--description-color)" }}
         >
-          If you want to contribute to our open-source projects, there are
-          several projects and ways to contribute to each. To become a part of
-          our community, clone our GitHub repository and propose your changes.
-          Submit your alterations via pull requests for our evaluation. Once
-          accepted, you will receive the NFT below and gain access to our
-          members-only chatroom for further collaboration.
+          Join Crypto Policy DAO to enhance regulatory clarity and promote
+          blockchain innovation
         </p>
-        <p
-          className="mx-auto text-center text-xl"
-          style={{
-            color: "var(--content-color)",
-            maxWidth: "900px",
-          }}
-        >
-          As the DAO earns revenue for its crypto lobbying efforts, all members
-          will split the DAO&apos;s treasury every quarter, and you can view the
-          current revenue under the Treasury tab.
-        </p>
-      </div>
-      <div className="container flex max-w-[40rem] flex-col items-center gap-4">
-        <video autoPlay loop muted style={{ borderRadius: "20px" }}>
-          <source src="/images/MemberNFT.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <div>
+          <p
+            className="mx-auto mb-10 text-center text-xl"
+            style={{
+              color: "var(--content-color)",
+              maxWidth: "900px",
+            }}
+          >
+            There are two ways to join Crypto Policy DAO; you can either
+            contribute financially or contribute to our open-source projects.
+            You can find more information about financially supporting the DAO
+            under the Support tab, though more information about contributing to
+            our open-source projects is provided below.
+          </p>
+          <p
+            className="mx-auto mb-10 text-center text-xl"
+            style={{
+              color: "var(--content-color)",
+              maxWidth: "900px",
+            }}
+          >
+            If you want to contribute to our open-source projects, there are
+            several projects and ways to contribute to each. To become a part of
+            our community, clone our GitHub repository and propose your changes.
+            Submit your alterations via pull requests for our evaluation. Once
+            accepted, you will receive the NFT below and gain access to our
+            members-only chatroom for further collaboration.
+          </p>
+          <p
+            className="mx-auto text-center text-xl"
+            style={{
+              color: "var(--content-color)",
+              maxWidth: "900px",
+            }}
+          >
+            As the DAO earns revenue for its crypto lobbying efforts, all
+            members will split the DAO&apos;s treasury every quarter, and you
+            can view the current revenue under the Treasury tab.
+          </p>
+        </div>
+        <div className="container flex max-w-[40rem] flex-col items-center gap-4">
+          <video autoPlay loop muted style={{ borderRadius: "20px" }}>
+            <source src="/images/MemberNFT.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
     </div>
   )
--- app/(marketing)/matrix/page.tsx
+++ app/(marketing)/matrix/page.tsx
@@ -5,83 +5,85 @@ import MatrixLongForm from "./MatrixLongForm"
 
 export default function Contribute() {
   return (
-    <div>
-      <h1
-        className="mb-5 mt-20 text-center"
-        style={{ color: "var(--header-color)", fontSize: "2rem" }}
-      >
-        Blockchain Regulation Matrix
-      </h1>
-      <div>
-        <p
-          className="mx-auto my-10 text-center"
-          style={{
-            color: "var(--description-color)",
-            fontSize: "1.5rem",
-            maxWidth: "900px",
-          }}
+    <div className="quando-regular">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+        <h1
+          className="mb-5 mt-20 text-center"
+          style={{ color: "var(--header-color)", fontSize: "2rem" }}
         >
-          The Blockchain Regulation Matrix (BRM) aims to construct a
-          comprehensive framework outlining blockchain&apos;s regulation
-          aspects. It delves into centralized and decentralized protocols,
-          emphasizing compliance without compromising blockchain ethos. This
-          matrix categorizes technology stacks and invites contributions to
-          assess each layer&apos;s government concerns, consumer risks,
-          regulatory pros, and cons.
-        </p>
+          Blockchain Regulation Matrix
+        </h1>
+        <div>
+          <p
+            className="mx-auto my-10 text-center"
+            style={{
+              color: "var(--description-color)",
+              fontSize: "1.5rem",
+              maxWidth: "900px",
+            }}
+          >
+            The Blockchain Regulation Matrix (BRM) aims to construct a
+            comprehensive framework outlining blockchain&apos;s regulation
+            aspects. It delves into centralized and decentralized protocols,
+            emphasizing compliance without compromising blockchain ethos. This
+            matrix categorizes technology stacks and invites contributions to
+            assess each layer&apos;s government concerns, consumer risks,
+            regulatory pros, and cons.
+          </p>
+          <br></br>
+          <p
+            className="mx-auto mb-10 text-center"
+            style={{
+              color: "var(--content-color)",
+              maxWidth: "900px",
+              fontSize: "1rem",
+            }}
+          >
+            This initiative aims to spotlight areas of concern, particularly
+            focusing on aspects related to the Howie test&apos;s significance in
+            determining whether a token qualifies as a security or commodity.
+            However, paramount to all objectives is the identification of spaces
+            within blockchain technology that adhere to its ethos while ensuring
+            regulatory compliance.
+          </p>
+          <br></br>
+          <p
+            className="mx-auto mb-10 text-center"
+            style={{
+              color: "var(--content-color)",
+              maxWidth: "900px",
+              fontSize: "1rem",
+            }}
+          >
+            The BRM comprises a conglomerate of technology and resources
+            constituting the foundational structure of the blockchain and all
+            its derivatives. Initially, the BRM dissects the blockchain stack
+            into two core categories: centralized and decentralized.
+            Subsequently, each category is subjected to a predefined set of
+            inquiries across various layers of the blockchain stack.
+          </p>
+        </div>
         <br></br>
-        <p
-          className="mx-auto mb-10 text-center"
+        <div
+          className="mx-auto w-full max-w-lg rounded-md px-4 py-3"
           style={{
-            color: "var(--content-color)",
-            maxWidth: "900px",
+            color: "var(--sub-header-color)",
+            backgroundColor: "var(--bac4)",
             fontSize: "1rem",
           }}
         >
-          This initiative aims to spotlight areas of concern, particularly
-          focusing on aspects related to the Howie test&apos;s significance in
-          determining whether a token qualifies as a security or commodity.
-          However, paramount to all objectives is the identification of spaces
-          within blockchain technology that adhere to its ethos while ensuring
-          regulatory compliance.
-        </p>
-        <br></br>
-        <p
-          className="mx-auto mb-10 text-center"
-          style={{
-            color: "var(--content-color)",
-            maxWidth: "900px",
-            fontSize: "1rem",
-          }}
-        >
-          The BRM comprises a conglomerate of technology and resources
-          constituting the foundational structure of the blockchain and all its
-          derivatives. Initially, the BRM dissects the blockchain stack into two
-          core categories: centralized and decentralized. Subsequently, each
-          category is subjected to a predefined set of inquiries across various
-          layers of the blockchain stack.
-        </p>
+          <p>
+            There are two illustrations of the Blockchain Regulation Matrix
+            below, a short-form immediately below and a long-form after.
+          </p>
+          <br></br>
+          <p>Hover over each of the images below to read more information.</p>
+        </div>
+        <Matrix />
+        <br />
+        <br />
+        <MatrixLongForm />
       </div>
-      <br></br>
-      <div
-        className="mx-auto w-full max-w-lg rounded-md px-4 py-3"
-        style={{
-          color: "var(--sub-header-color)",
-          backgroundColor: "var(--bac4)",
-          fontSize: "1rem",
-        }}
-      >
-        <p>
-          There are two illustrations of the Blockchain Regulation Matrix below,
-          a short-form immediately below and a long-form after.
-        </p>
-        <br></br>
-        <p>Hover over each of the images below to read more information.</p>
-      </div>
-      <Matrix />
-      <br />
-      <br />
-      <MatrixLongForm />
     </div>
   )
 }
