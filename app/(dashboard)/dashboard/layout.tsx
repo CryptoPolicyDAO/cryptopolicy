d801f540c4844ebef4317412efc02d4554cb7e03
Michael <enjoyweaver@gmail.com>
2024-03-10 12:58:28 -0500

    add blog post functionality back

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,11 @@
+import { notFound } from "next/navigation"
+import { createClient } from "@/utils/supabase/server"
+
 import { dashboardConfig } from "@/config/dashboard"
 import { MainNav } from "@/components/main-nav"
 import { DashboardNav } from "@/components/nav"
 import { SiteFooter } from "@/components/site-footer"
+import { UserAccountNav } from "@/components/user-account-nav"
 
 interface DashboardLayoutProps {
   children?: React.ReactNode
@@ -10,11 +14,24 @@ interface DashboardLayoutProps {
 export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
+  const supabase = createClient()
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    return notFound()
+  }
+
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex h-16 items-center justify-between py-4">
           <MainNav items={dashboardConfig.mainNav} />
+          <UserAccountNav
+            user={{
+              name: data.user.name,
+              image: data.user.image,
+              email: data.user.email,
+            }}
+          />
         </div>
       </header>
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
