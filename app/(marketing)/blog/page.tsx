0159ad1141cf5b85295e54aeb761650b5a1b7ac7
Michael <enjoyweaver@gmail.com>
2024-03-10 00:56:58 -0600

    blog and details

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -20,7 +20,7 @@ export default async function BlogPage() {
     <div className="quando-regular container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="quando-regular inline-block font-heading text-4xl tracking-tight lg:text-5xl">
+          <h1 className="quando-regular inline-block text-4xl tracking-tight lg:text-5xl">
             Blog
           </h1>
           <p className="text-xl text-muted-foreground">
@@ -40,8 +40,8 @@ export default async function BlogPage() {
                 <Image
                   src={post.image}
                   alt={post.title}
-                  width={804}
-                  height={452}
+                  width={300}
+                  height={300}
                   className="rounded-md border bg-muted transition-colors"
                   priority={index <= 1}
                 />
